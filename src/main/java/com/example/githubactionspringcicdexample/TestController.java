a3b6ff705abeda1962c24fa2a05c9f693c790f3e
howisitgoing <howisitgoing@kakao.com>
2023-11-10 01:58:50 +0900

    feat(dockerFile):
    도커 파일 생성

--- src/main/java/com/example/githubactionspringcicdexample/TestController.java
+++ src/main/java/com/example/githubactionspringcicdexample/TestController.java
@@ -1,2 +1,13 @@
-package com.example.githubactionspringcicdexample;public class TestController {
+package com.example.githubactionspringcicdexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
 }
