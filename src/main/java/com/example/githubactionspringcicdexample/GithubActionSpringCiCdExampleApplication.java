a669bcae5ef5de481b1615b1169dba66c6bf5b58
howisitgoing <howisitgoing@kakao.com>
2023-11-10 01:56:33 +0900

    Initial commit

---
+++ src/main/java/com/example/githubactionspringcicdexample/GithubActionSpringCiCdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionspringcicdexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionSpringCiCdExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionSpringCiCdExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionspringcicdexample/GithubActionSpringCiCdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionspringcicdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionSpringCiCdExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
